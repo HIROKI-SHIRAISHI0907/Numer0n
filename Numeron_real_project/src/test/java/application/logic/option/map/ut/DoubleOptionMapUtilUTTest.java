66fef30225fd5e95c122637646e3ff177b2b4695
shiraishi0907 <shiraishi0907github.com>
2024-03-08 12:55:36 +0900

    Map Test commit

---
+++ Numeron_real_project/src/test/java/application/logic/option/map/ut/DoubleOptionMapUtilUTTest.java
@@ -0,0 +1,21 @@
+package application.logic.option.map.ut;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+/**
+ * <p>
+ * ダブルオプションマップテストクラス
+ * </p>
+ * @author shiraishitoshio
+ *
+ */
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+@Tag("UT")
+public class DoubleOptionMapUtilUTTest {
+
+}
